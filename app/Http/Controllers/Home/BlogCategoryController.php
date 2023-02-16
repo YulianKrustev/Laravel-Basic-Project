9a16b03b9aec8c641b499af8e3f49616fe427ba1
Julian Krustev <juliankrustev2018@gmail.com>
2023-02-16 19:02:31 +0200

    Added backend blog category page setup

---
+++ app/Http/Controllers/Home/BlogCategoryController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Models\BlogCategory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
+
+class BlogCategoryController extends Controller
+{
+    public function AllBlogCategory(){
+
+        $blogcategory = BlogCategory::latest()->get();
+       
+        return view('admin.blog_category.blog_category_all', compact('blogcategory'));
+
+    }   // End Method
+
+    public function AddBlogCategory(){
+
+          return view('admin.blog_category.blog_category_add');
+
+    } //End Method
+
+    public function StoreBlogCategory(Request $request){
+
+        $request->validate([
+            'blog_category' => 'required'
+        ]);
+
+        BlogCategory::insert([
+            'blog_category' => $request->blog_category,
+            'created_at' => Carbon::now()
+        ]);
+
+        $notification = [
+                'message' => 'Blog Category Inserted Successfully',
+                'alert-type' => 'success'
+            ];
+
+        return redirect()->route('all.blog.category')->with($notification);
+    } //End Method
+
+
+    public function EditBlogCategory($id){
+
+        $blogcategory = BlogCategory::findOrFail($id);
+        return view('admin.blog_category.blog_category_edit', compact('blogcategory'));
+
+    } //End Method
+
+
+    public function UpdateBlogCategory(Request $request, $id){
+
+        $blogcategory = BlogCategory::findOrFail($id)->update([
+            'blog_category' => $request->blog_category
+        ]);
+
+        $notification = [
+                'message' => 'Blog Category Updated Successfully',
+                'alert-type' => 'success'
+            ];
+
+        return redirect()->route('all.blog.category')->with($notification);
+
+    } //End Method
+
+    public function DeleteBlogCategory($id) {
+
+        BlogCategory::findOrFail($id)->delete();
+
+        $notification = [
+                'message' => 'Blog Category Deleted Successfully',
+                'alert-type' => 'success'
+            ];
+
+        return redirect()->back()->with($notification);
+
+    } //End Method
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\HomeSliderController;
 use App\Http\Controllers\Home\AboutController;
 use App\Http\Controllers\Home\PortfolioController;
+use App\Http\Controllers\Home\BlogCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -70,6 +71,18 @@ Route::controller(PortfolioController::class)->group(function () {
     Route::get('/delete/portfolio/{id}', 'DeletePortfolio')->name('delete.portfolio');
     Route::post('/update/portfolio/', 'UpdatePortfolio')->name('update.portfolio');
     Route::get('/portfolio/details/{id}', 'PortfolioDetails')->name('portfolio.details');
+    
+});
+
+// Blog Category All Routes
+Route::controller(BlogCategoryController::class)->group(function () {
+    Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');
+    Route::get('/add/blog/category', 'AddBlogCategory')->name('add.blog.category');
+    Route::post('/store/blog/category', 'StoreBlogCategory')->name('store.blog.category');
+    Route::get('/edit/blog/category/{id}', 'EditBlogCategory')->name('edit.blog.category');
+    Route::post('/update/blog/category/{id}', 'UpdateBlogCategory')->name('update.blog.category');
+    Route::get('/delete/blog/category/{id}', 'DeleteBlogCategory')->name('delete.blog.category');
+  
     
 });
 
