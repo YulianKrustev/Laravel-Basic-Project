c6d3d941ed885ac3515d5629bd2340a1dfc06b8c
Julian Krustev <juliankrustev2018@gmail.com>
2023-02-24 22:55:24 +0200

    Added backend contact message page

--- app/Http/Controllers/Home/ContactController.php
+++ app/Http/Controllers/Home/ContactController.php
@@ -15,4 +15,43 @@ class ContactController extends Controller
         return view('frontend.contact');
 
     } // End Method
+
+
+    public function StoreMessage(Request $request){
+        Contact::insert([
+            'name' => $request->name,
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'phone' => $request->phone,
+            'message' => $request->message,
+            'created_at' => Carbon::now()
+            
+        ]);
+
+       $notification = [
+                'message' => 'Your Message Submited Successfully',
+                'alert-type' => 'success'
+            ];
+
+        return redirect()->back()->with($notification);
+    } // End Method
+
+
+    public function ContactMessage(){
+        $contacts = Contact::latest()->get();
+        return view('admin.contact.allcontact', compact('contacts'));
+    } // End Method
+
+
+    public function DeleteMessage($id){
+
+        Contact::findOrFail($id)->delete();
+
+         $notification = [
+                'message' => 'Your Message Deleted Successfully',
+                'alert-type' => 'success'
+            ];
+
+            return redirect()->back()->with($notification);
+    } // End Method
 }
