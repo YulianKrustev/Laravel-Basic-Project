1eac1e00e64f51c6a61806b2ec9d6619e1cbfe75
Julian Krustev <juliankrustev2018@gmail.com>
2023-02-25 17:59:35 +0200

    Added frontend navbar portfolio page

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -141,4 +141,9 @@ class PortfolioController extends Controller
             return view('frontend.portfolio_details', compact('portfolio'));
 
     } // End Method
+
+    public function HomePortfolio(){
+        $portfolio = Portfolio::latest()->get();
+        return view('frontend.portfolio', compact('portfolio'));
+    }  // End Method
 }
